48919df0ae7d420b9ebed24abf3e5712fd770dfb
Axel <axacorro@gmail.com>
2021-12-12 18:00:50 -0800

    create view controllers

---
+++ DSVocabulary/DSVocabulary/DefinitionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DefinitionViewController.swift
+//  DSVocabulary
+//
+//  Created by Axel Corro on 12/12/21.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
