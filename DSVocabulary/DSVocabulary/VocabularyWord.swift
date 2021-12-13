78ee739d1c2434ca7d56d024b932b92ab80bad23
Axel <axacorro@gmail.com>
2021-12-12 19:41:31 -0800

    create a VocabularyWord model struct

---
+++ DSVocabulary/DSVocabulary/VocabularyWord.swift
@@ -0,0 +1,13 @@
+//
+//  VocabularyWord.swift
+//  DSVocabulary
+//
+//  Created by Axel Corro on 12/12/21.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
