b8c9577f52e347457ff006d39cc26659ec44b7dd
Axel <axacorro@gmail.com>
2021-12-12 20:11:13 -0800

    implements wordstableviewcontroller

--- DSVocabulary/DSVocabulary/WordsTableTableViewController.swift
+++ DSVocabulary/DSVocabulary/WordsTableTableViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class WordsTableTableViewController: UITableViewController {
+    
+    var vocabWords: [VocabularyWord] = [
+        VocabularyWord(word: "Data Science", definition: "Data science is an interdisciplinary field that uses scientific methods, processes, algorithms and systems to extract knowledge and insights from noisy, structured and unstructured data,and apply knowledge and actionable insights from data across a broad range of application domains.")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,25 +25,24 @@ class WordsTableTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return vocabWords.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
 
-        // Configure the cell...
+        let vocab = vocabWords[indexPath.row]
+        cell.textLabel?.text = vocab.word
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -76,14 +79,16 @@ class WordsTableTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowDefinitionSegue" {
+            guard let definitionVC = segue.destination as? DefinitionViewController,
+                  let indexPath = tableView.indexPathForSelectedRow else {return}
+            let vocab = vocabWords[indexPath.row]
+            definitionVC.vocabWords = vocab
+        }
     }
-    */
 
 }
